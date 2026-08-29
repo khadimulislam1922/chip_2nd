eca13a06369a4607851c07d8e4d6d155077811fe
khadimulislam1922 <khadimulislam1922@gmail.com>
2026-08-29 09:35:03 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,32 +1,28 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, ClockCycles
+from cocotb.triggers import RisingEdge, ClockCycles, FallingEdge
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Starting Counter Test")
 
-    # Set initial values
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     
-    # Create a 10us clock
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset the counter
     dut._log.info("Reset")
-    dut.rst_n.value = 0  # Active low reset
+    dut.rst_n.value = 0  
     await ClockCycles(dut.clk, 5)
-    dut.rst_n.value = 1  # Stop reset
+    dut.rst_n.value = 1  
 
     dut._log.info("Checking counter behavior")
     
-    # Wait for 3 clock cycles
     await ClockCycles(dut.clk, 3)
+    await FallingEdge(dut.clk) # লজিক গেট সেটেল হওয়ার জন্য অপেক্ষা
     
-    # Since we waited 3 cycles, the counter should be exactly at 3
-    assert dut.uo_out.value == 3
+    assert int(dut.uo_out.value) == 3
     
     dut._log.info("Test passed successfully!")
